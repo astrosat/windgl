8cb4ddad03dc330972a5c74da1e13808adc1cedc
Jakub Hampl <kopomir@gmail.com>
2018-11-09 16:01:25 +0000

    Adds a better rollup plugin that compresses shader source

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,13 @@
-import string from 'rollup-plugin-string';
-import buble from 'rollup-plugin-buble';
+import glsl from "rollup-plugin-glsl";
+import buble from "rollup-plugin-buble";
 import pkg from "./package.json";
 
 export default {
-    input: 'src/index.js',
-    output: [
-        {file: pkg.browser, format: 'umd', name: 'windGL'},
-        {file: pkg.main, format: 'cjs'},
-        {file: pkg.module, format: 'es'}
-    ],
-    plugins: [
-        string({include: './src/shaders/*.glsl'}),
-        buble()
-    ]
+  input: "src/index.js",
+  output: [
+    { file: pkg.browser, format: "umd", name: "windGL" },
+    { file: pkg.main, format: "cjs" },
+    { file: pkg.module, format: "es" }
+  ],
+  plugins: [glsl({ include: "./src/shaders/*.glsl" }), buble()]
 };
